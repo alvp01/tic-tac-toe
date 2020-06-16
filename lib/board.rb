d9dbf297c9efaa566f590237f3b2806f8ae12b3d
Shingirayi Mandebvu <shingim1@gmail.com>
2020-06-16 18:57:50 +0200

    Added basic game flow

--- lib/board.rb
+++ lib/board.rb
@@ -1,28 +1,25 @@
 class Board
-    attr_accessor :board
+  attr_accessor :board
 
-    def initialize
-        @board = Array(1..9)
-    end
+  def initialize
+    @board = Array(1..9)
+  end
 
-    def display_board
-        puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
-        puts '----------'
-        puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
-        puts '----------'
-        puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
-    end
+  def display_board
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts '----------'
+    puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+    puts '----------'
+    puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
+  end
 
-    def update_board(ind, symb)
-        @board[ind] = symb
-    end
+  def update_board(ind, symb)
+    @board[ind] = symb
+  end
 
-    def input_valid?(n)
-        if (n.is_a? Integer)
-            if @board.include?(n)
-                true
-            end  
-        end
-        false
-    end
+  def input_valid?(user_input)
+    return true if @board.include?(user_input)
+
+    false
+  end
 end

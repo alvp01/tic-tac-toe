6f39165ab3fd20b04699575ce970271aa23eefbe
Shingirayi Mandebvu <shingim1@gmail.com>
2020-06-16 22:07:13 +0200

    Added function to play game

--- bin/main.rb
+++ bin/main.rb
@@ -4,6 +4,7 @@ require_relative '../lib/helpers.rb'
 require_relative '../lib/board.rb'
 require_relative '../lib/player.rb'
 
+
 helpers = Helpers.new
 
 puts 'Welcome to Tic Tac Toe !'
@@ -11,55 +12,46 @@ puts 'Player 1, what do you want to use X or O?'
 player_1_sym = gets.strip.upcase
 player_2_sym = ''
 game_board = Board.new
+player_1 = nil
+player_2 = nil
+
 loop do
   if helpers.valid_symbol?(player_1_sym)
     # User input validated. Player selected an X or an O
     puts 'Ready Player 1 !'
     player_2_sym = player_1_sym == 'X' ? 'O' : 'X'
     p "Player 2 is now #{player_2_sym}"
-    # player_1 = Player.new(player_1_sym)
-    # player_2 = Player.new(player_2_sym)
+    player_1 = Player.new("Player 1", player_1_sym)
+    player_2 = Player.new("Player 2", player_2_sym)
     break
   else
     puts "Enter either an X or an O. Not #{player_1_sym}".red
     player_1_sym = gets.strip.upcase
   end
 end
+
 game_board.display_board
-## Player selects a cell depending on who the player is, and redraw the board
+
+def play_game(game_board, player)
+  puts "#{player.player_name}. Whats your move?".red
+  p 'Enter a number between 1 and 9.'
+  player_move = gets.strip
+  until game_board.input_valid?(player_move.to_i)
+    puts '#{player_name}. Whats your move?'.green
+    puts 'Remember to enter a number between 1 and 9.'.red
+    player_move = gets.strip
+    p "#{player_move} move in the loop"
+  end
+  game_board.update_board(player_move.to_i - 1, player.player_symbol)
+end
+
+
 moves_counter = 0
 while moves_counter < 9
   if moves_counter.even?
-    # #Player 1's Turn
-    ## Get input
-    puts 'Player 1. Whats your move?'.red
-    p 'Enter a number between 1 and 9.'
-    player_1_move = gets.strip
-    ## Validate Input
-    until game_board.input_valid?(player_1_move.to_i)
-      puts 'Player 1. Whats your move?'.green
-      puts 'Remember to enter a number between 1 and 9.'.red
-      player_1_move = gets.strip
-      p "#{player_1_move} move in the loop"
-    end
-    ## Update Board
-    game_board.update_board(player_1_move.to_i - 1, player_1_sym)
-  ## Redraw
+    play_game(game_board,player_1)
   else
-    # #Player 2's Turn
-    ## Get input
-    puts 'Player 2. Whats your move?'.brown
-    p 'Enter a number between 1 and 9.'
-    player_2_move = gets.strip
-    ## Validate Input
-    until game_board.input_valid?(player_2_move.to_i)
-      puts 'Player 2. Whats your move?'.green
-      puts 'Remember to enter a number between 1 and 9.'.red
-      player_2_move = gets.strip
-      p "#{player_2_move} move in the loop"
-    end
-    ## Update Board
-    game_board.update_board(player_2_move.to_i - 1, player_2_sym)
+    play_game(game_board,player_2)
   end
   p
   game_board.display_board

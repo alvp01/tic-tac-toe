24c24fc2dd30cac8da6c4013452e9f225fd6483b
Michael Threels <michaelthreels@gmail.com>
2020-06-26 19:19:53 +0200

    added tests for method input_valid?

--- spec/tic-tac-toe_spec.rb
+++ spec/tic-tac-toe_spec.rb
@@ -4,8 +4,26 @@ describe Board do
   let(:game_board) { Board.new }
 
   describe '#move_made?' do
-    it 'returns true if the input for the space is valid, otherwise false' do
-      expect(p game_board.move_made?(1)).to eql(false)
+    context'returns true if the input for the space is already ocupied, otherwise false' do
+      it 'test if position is not yet ocupied' do
+        expect(game_board.move_made?(1)).to eql(false)
+      end
+
+      it 'test if position is already ocupied' do
+        game_board.board[1] = 'X'
+        expect(game_board.move_made?(1)).to eql(true)
+      end
+    end
+  end
+
+  describe '#input_valid?' do
+    context 'checks if the move the player entered is inside the board parameters'
+      it 'returns true if the move is inside the parameters' do
+        expect(game_board.input_valid?(1)).to eql(true)
+      end
+      it 'returns false if the move is outside the parameters' do
+        expect(game_board.input_valid?(10)).to eql(false)
+      end
     end
   end
 end
